f31e4f597cccfa285646ef726295bcd922af5f3a
Mayank-ShopTrade <mayank.pant@shoptrade.co>
2020-04-21 00:39:28 +0530

    Updates

---
+++ precache-manifest.ddb23bcd057b34cecc91e11b4081f59a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cc819109d28f509496081eb2518b61",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "3394cb50662282e07f85",
+    "url": "/covid-19/static/css/main.94433fa4.chunk.css"
+  },
+  {
+    "revision": "8aa06934d9b9214b3332",
+    "url": "/covid-19/static/js/2.7e081eab.chunk.js"
+  },
+  {
+    "revision": "89093d4a6fb817ae3a1001b67bd088d4",
+    "url": "/covid-19/static/js/2.7e081eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3394cb50662282e07f85",
+    "url": "/covid-19/static/js/main.462e959d.chunk.js"
+  },
+  {
+    "revision": "e8c41c5b296c73307877",
+    "url": "/covid-19/static/js/runtime-main.23cfc567.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/covid.d7265326.png"
+  }
+]);
